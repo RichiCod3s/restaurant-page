d4b16d68b3f8cfa456db4cb113a542d810f3a119
Aaron Richardson <aaronrichardsonsg@gmail.com>
2024-10-20 16:21:33 +0800

    clean up code

--- src/index.js
+++ src/index.js
@@ -4,22 +4,30 @@ import { greeting } from "./anotherFile.js";
 console.log(greeting);
 
 let content = document.querySelector("#content");
-let heading = document.createElement("h1");
-let description = document.createElement("p");
-let openingHours = document.createElement("div");
 
-heading.classList.add("heading");
-description.classList.add("description");
-openingHours.classList.add("openingHours");
+// Helper function to create an element with classes and text content
+function createElementWithClass(tag, className, innerHTML = "") {
+  let element = document.createElement(tag);
+  element.classList.add(className);
+  element.innerHTML = innerHTML;
+  return element;
+}
 
-heading.textContent = "Aileen's Breakfast Place";
-description.textContent="Soak up the sea views and enjoy a selection of breakfast treats at Aileen's Breakfast Place. We offer a number of different Filipino breakfast specialties, including serveral types of glutenous rice dishes served with local cocoa. Sit down and enjoy a hearty local breakfast to start your day off right! Tops Sarap!";
+// Create elements
+let heading = createElementWithClass("h1", "heading", "Aileen's Breakfast Place");
+let description = createElementWithClass("p", "description", "Soak up the sea views and enjoy a selection of breakfast treats at Aileen's Breakfast Place. We offer a number of different Filipino breakfast specialties, including several types of glutenous rice dishes served with local cocoa. Sit down and enjoy a hearty local breakfast to start your day off right! Tops Sarap!");
+let openingHours = createElementWithClass("div", "openingHours");
 
-let openingHoursTitle = document.createElement("h2");
-openingHoursTitle.textContent = "Hours";
-let monday = document.createElement("p");
+// Creating nested elements for opening hours
+let openingHoursTitle = createElementWithClass("h2", "openingHoursTitle", "Hours");
+let times = createElementWithClass("div", "times", "Sunday: 8am - 8pm <br> Monday: 6am - 6pm <br> Tuesday: 6am - 6pm<br> Wednesday: 6am - 6pm<br> Thursday: 6am - 6pm<br> Friday: 6am - 6pm<br> Saturday: 8am - 8pm");
 
+
+// Append nested elements
 openingHours.appendChild(openingHoursTitle);
+openingHours.appendChild(times);
+
+// Append everything to the content container
 content.appendChild(heading);
 content.appendChild(description);
 content.appendChild(openingHours);
